e41a589c443b3ef44e1f8c83fbc5729d966e4a8a
SortTechSupport <153835825+SortTechSupport@users.noreply.github.com>
2024-01-16 15:25:29 +0000

    Update WindowsSetupInitialise.ps1

--- WindowsSetupInitialise.ps1
+++ WindowsSetupInitialise.ps1
@@ -5,11 +5,13 @@ New-Item -ItemType Directory -Path $rootPath\Logs -Force
 Set-Location -Path $rootPath
 
 # Pull setup script from Github and run it 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Kisageru/WindowsSetup/main/Windows-Setup.ps1 -OutFile ".\Windows-Setup.ps1"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Kisageru/WindowsSetup/main/ExecuteSaraCmd.ps1 -OutFile ".\ExecuteSaraCmd.ps1"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Kisageru/WindowsSetup/main/Install-Office365Suite.ps1 -OutFile ".\Install-Office365Suite.ps1"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Kisageru/WindowsSetup/main/UserBasedLicencingConfiguration.xml -Outfile ".\UserBasedLicencingConfiguration.xml"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/WindowsSetup.ps1 -OutFile ".\WindowsSetup.ps1"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/ExecuteSaraCmd.ps1 -OutFile ".\ExecuteSaraCmd.ps1"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/Install-Office365Suite.ps1 -OutFile ".\Install-Office365Suite.ps1"
+#Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/Windows-Setup/main/OfficeSetup/UserBasedLicencingConfiguration.xml -Outfile ".\UserBasedLicencingConfiguration.xml"
+Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\WindowsSetup.xml" -Destination $rootPath
+Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\UserBasedLicencingConfiguration.xml" -Destination $rootPath
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409" -OutFile ".\TeamsBootStrapper.exe"
 
 # Run the Windows-Setup Script
-& .\Windows-Setup.ps1
+& .\WindowsSetup.ps1
