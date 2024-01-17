709d57bc2233a3def7daa45eccc1a7d926b5229e
SortTechSupport <153835825+SortTechSupport@users.noreply.github.com>
2024-01-17 15:32:29 +0000

    Update Install-Office365Suite.ps1

--- OfficeSetup/Install-Office365Suite.ps1
+++ OfficeSetup/Install-Office365Suite.ps1
@@ -1,7 +1,6 @@
-
-#Downloads and installs the Office 365 suite for Windows using the Office Deployment Tool
-#To use - invoke this script and supply the path for the .xml file
-# eg. .\Install-Office365Suite.ps1 -ConfigurationXMLFile "\\vfp02\sortgroup\Scripts\OfficeSetup\OfficeInstall\UserBasedLicencingConfiguration.xml"
+# Downloads and installs the Office 365 suite for Windows using the Office Deployment Tool
+# To use - invoke this script and supply the path for the .xml file
+# eg. .\Install-Office365Suite.ps1 -ConfigurationXMLFile "\\Path\To\UserBasedLicencingConfiguration.xml"
 
 Function Test-URL{
   Param(
@@ -24,22 +23,21 @@ Function Test-URL{
   }    
   Return $True
 }
-Function Get-ODTURL {
-  $ODTDLLink = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16130-20218.exe"
 
-  If((Test-URL -CurrentURL $ODTDLLink) -eq $False){
-    $MSWebPage = (Invoke-WebRequest "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117" -UseBasicParsing).Content
-  
-    $MSWebPage | ForEach-Object {
-      If ($_ -match "url=(https://.*officedeploymenttool.*\.exe)"){
-        $ODTDLLink = $matches[1]}
-      }
+function Get-ODTURL {
+
+  [String]$MSWebPage = Invoke-RestMethod 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
+
+  $MSWebPage | ForEach-Object {
+    if ($_ -match 'url=(https://.*officedeploymenttool.*\.exe)') {
+      $matches[1]
+    }
   }
-  Return $ODTDLLink
 }
 
 $VerbosePreference = "Continue"
 $ErrorActionPreference = "Stop"
+
 
 $CurrentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 If(!($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){
@@ -47,8 +45,8 @@ If(!($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
     Write-Warning "Please rerun this script as Administrator."
     Exit
 }
-
-$OfficeInstallDownloadPath = "C:\temp\OfficeInstall"
+$ConfigurationXMLFile = "$PSScriptRoot\UserBasedLicencingConfiguration.xml"
+$OfficeInstallDownloadPath = "$PSScriptRoot\OfficeInstall"
 
 If(-Not(Test-Path $OfficeInstallDownloadPath )){
   New-Item -Path $OfficeInstallDownloadPath  -ItemType Directory -ErrorAction Stop | Out-Null
@@ -79,33 +77,10 @@ Try{
 }
 
 #Run the O365 install
-$ConfigurationXMLFile = "\\vfp02\sortgroup\Scripts\OfficeSetup\OfficeInstall\UserBasedLicencingConfiguration.xml"
 Try{
   Write-Verbose "Downloading and installing Office 365"
-  #$OfficeInstall = Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfiguratonXMLFile" -Wait -PassThru
   Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfigurationXMLFile" -Wait -PassThru
 }Catch{
   Write-Warning "Error running the Office install. The error is below:"
   Write-Warning $_
-}
-
-
-#Check if Office 365 suite was installed correctly.
-
-$RegLocations = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-                  'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-                 )
-
-$OfficeInstalled = $False
-Foreach ($Key in (Get-ChildItem $RegLocations) ) {
-  If($Key.GetValue("DisplayName") -like "*Office 365*") {
-    $OfficeVersionInstalled = $Key.GetValue("DisplayName")
-    $OfficeInstalled = $True
-  }
-}
-
-If($OfficeInstalled){
-  Write-Verbose "$($OfficeVersionInstalled) installed successfully!"
-}Else{
-  Write-Warning "Office 365 was not detected after the install ran"
 }
