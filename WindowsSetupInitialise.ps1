6b9c75361a720cd8406f8e480595c43c2d7b1c56
SortTechSupport <153835825+SortTechSupport@users.noreply.github.com>
2024-01-17 15:30:42 +0000

    Update WindowsSetupInitialise.ps1

--- WindowsSetupInitialise.ps1
+++ WindowsSetupInitialise.ps1
@@ -8,7 +8,6 @@ Set-Location -Path $rootPath
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/WindowsSetup.ps1 -OutFile ".\WindowsSetup.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/ExecuteSaraCmd.ps1 -OutFile ".\ExecuteSaraCmd.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/Install-Office365Suite.ps1 -OutFile ".\Install-Office365Suite.ps1"
-#Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/Windows-Setup/main/OfficeSetup/UserBasedLicencingConfiguration.xml -Outfile ".\UserBasedLicencingConfiguration.xml"
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\WindowsSetup.xml" -Destination $rootPath
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\UserBasedLicencingConfiguration.xml" -Destination $rootPath
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409" -OutFile ".\TeamsBootStrapper.exe"
