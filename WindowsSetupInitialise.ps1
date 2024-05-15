99d175d9baf81f379907c72eb6cdb89b1c4c71ec
SortTechSupport <153835825+SortTechSupport@users.noreply.github.com>
2024-05-15 17:24:51 +0100

    Update WindowsSetupInitialise.ps1
    add wildix to windows setup

--- WindowsSetupInitialise.ps1
+++ WindowsSetupInitialise.ps1
@@ -11,13 +11,14 @@ New-Item -ItemType Directory -Path $rootPath\Logs -Force
 Set-Location -Path $rootPath
 
 #Install Snipe Module for asset management
-Install-Module SnipeitPS -Force
-Update-Module SnipeitPS -Force
+Install-Module SnipeitPS
+Update-Module SnipeitPS
 
 # Pull setup script from Github and run it 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/WindowsSetup.ps1 -OutFile ".\WindowsSetup.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/ExecuteSaraCmd.ps1 -OutFile ".\ExecuteSaraCmd.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/Install-Office365Suite.ps1 -OutFile ".\Install-Office365Suite.ps1"
+Invoke-WebRequest -Url https://files.wildix.com/integrations/win/collaboration/Collaboration-x64.msi -OutFile ".\Collaboration-x64.msi"
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\WindowsSetup.xml" -Destination $rootPath
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\UserBasedLicencingConfiguration.xml" -Destination $rootPath
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409" -OutFile ".\TeamsBootStrapper.exe"
