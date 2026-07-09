2dd0871dd5fac233d52ab1254dbd141441f7de6b
SortTechSupport <153835825+SortTechSupport@users.noreply.github.com>
2026-07-09 16:58:13 +0100

    Modify WindowsSetupInitialise.ps1 to adjust script downloads
    Updated the script to include the UninstallOffice.xml file download and removed the ExecuteSaraCmd.ps1 download.

--- WindowsSetupInitialise.ps1
+++ WindowsSetupInitialise.ps1
@@ -16,8 +16,8 @@ Update-Module SnipeitPS
 
 # Pull setup script from Github and run it 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/WindowsSetup.ps1 -OutFile ".\WindowsSetup.ps1"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/ExecuteSaraCmd.ps1 -OutFile ".\ExecuteSaraCmd.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/main/OfficeSetup/Install-Office365Suite.ps1 -OutFile ".\Install-Office365Suite.ps1"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/SortTechSupport/WindowsSetup/refs/heads/main/OfficeSetup/UninstallOffice.xml -Outfile ".\UninstallOffice.xml"
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\WindowsSetup.xml" -Destination $rootPath
 Copy-Item "\\vfp02\software$\_Local installers\WindowsSetup\UserBasedLicencingConfiguration.xml" -Destination $rootPath
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409" -OutFile ".\TeamsBootStrapper.exe"
